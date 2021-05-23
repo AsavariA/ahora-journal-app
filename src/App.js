a420df747d815a4233482a1ac5c0b17f22e4240c
Asavari Ambavane <ambavaneasavari@gmail.com>
2021-05-23 12:47:23 +0530

    routes changed

--- src/App.js
+++ src/App.js
@@ -1,46 +1,15 @@
-import React,{useState, useEffect} from "react"
-import Journal from "./main-layout/Journal"
-import Login from './authentication/Login'
-import SignUp from './authentication/SignUp'
+import React from "react"
 import { BrowserRouter as Router, Route, Switch, withRouter} from 'react-router-dom';
 import Cal from "./main-layout/Components/BigCalendar";
+import Landing from './authentication/Landing';
 
 function App(){
-  const [user,setUser]=useState('');
-  const [toggleForm,setToggleForm]=useState(true);
-  const formMode = () => { setToggleForm(!toggleForm); };
-
-  const userState=()=>{
-    const data=localStorage.getItem('user');
-    const us=data!==null ? JSON.parse(data):null;
-    setUser(us);
-  }
-
-useEffect(()=>{
-  userState();
-},[]);
-
+  
     return (
         <Router>
             <Switch>
                 <Route exact path="/cal" component={withRouter(Cal)} />
-                    <div>
-            
-                  <>
-                    {user !== null ? (
-                      <>
-                        <Journal setUserState={() => setUser(null)}/>
-                      </>
-                    ) : (
-                       <>
-                       {toggleForm ? 
-                        (<Login loggedIn={(user) => setUser(user)} toggle={() => formMode()}/>) 
-                      : (<SignUp toggle={() => formMode()}/>)}
-          
-                   </>
-                    )} 
-                        </>
-                        </div>
+                <Route path="/" component={withRouter(Landing)} />    
             </Switch>
         </Router>
     )
--- src/authentication/Landing.js
+++ src/authentication/Landing.js
@@ -1,88 +1,43 @@
-import React,  { useState, useEffect } from "react"
-import "./Landing.css"
-import './Login.css';
-import SignUp from "./SignUp"
-import Login from"./Login"
-import fire from "./fire"
+import React,{useState, useEffect} from 'react'
+import Journal from "../main-layout/Journal"
+import Login from './Login'
+import SignUp from './SignUp'
 
-function Landing(props){
+const Landing = () => {
+  const [user,setUser]=useState('');
+  const [toggleForm,setToggleForm]=useState(true);
+  const formMode = () => { setToggleForm(!toggleForm); };
 
-  const {email, setEmail, password, setPassword, handleSignUp, handleLogin, emailError, passwordError, hasAccount, setHasAccount} = props;
+  const userState=()=>{
+    const data=localStorage.getItem('user');
+    const us=data!==null ? JSON.parse(data):null;
+    setUser(us);
+  }
 
-        return(
-            <div className="main">
-            <div className="col-1">
-              <div className="content">
-                <h1>Who you are tomorrow begins with what you do today.</h1>
-                <p>Join us for a rejuvenating experience!</p>
-              </div>
-            </div>
-            <div className="col-2">
-              {hasAccount?
-              (<div className="form-div">
-              <h1>Sign In</h1>
-              <form>
-                  <input 
-                      type="email"
-                      name="email"
-                      placeholder="Email"
-                      required
-                      value={email}
-                      onChange={(e)=>setEmail(e.target.value)}
-                  />
-                  <input 
-                      type="password"
-                      name="full-name"
-                      placeholder="Password (min 6 characters)"
-                      required
-                      value={password}
-                      onChange={(e)=>setPassword(e.target.value)}
-                  />
-              <button onClick={handleLogin} >Log In</button>
-              </form>
-              <p>Don't have an account? <span onClick={()=>setHasAccount(!hasAccount)}>Sign Up</span></p>
-          </div>
+useEffect(()=>{
+  userState();
+},[]);
 
-        ):(
-
-            <div className="form-div">
-              <h1>Sign Up</h1>
-              <form>
-                  <input 
-                      type="text"
-                      name="full-name"
-                      placeholder="Full name"
-                  />
-                  <input 
-                      type="email"
-                      name="email"
-                      placeholder="Email"
-                      required
-                      value={email}
-                      onChange={(e)=>setEmail(e.target.value)}
-                  />
-                  <input 
-                      type="password"
-                      name="full-name"
-                      placeholder="Password (min 6 characters)"
-                      required
-                      value={password}
-                      onChange={(e)=>setPassword(e.target.value)}
-                  />
-                  <input 
-                      type="password"
-                      name="full-name"
-                      placeholder="Confirm Password"
-                  />
-              <button onClick={handleSignUp} >Register</button>
-              </form>
-              <p>Have an account? <span onClick={()=>setHasAccount(!hasAccount)}>Sign In</span></p>
-          </div>
-          )
-        }     
-            </div>
-          </div>
-        )
+  return (
+    <div>
+      <div>
+            <>
+              {user !== null ? (
+                <>
+                  <Journal setUserState={() => setUser(null)}/>
+                </>
+              ) : (
+                 <>
+                 {toggleForm ? 
+                  (<Login loggedIn={(user) => setUser(user)} toggle={() => formMode()}/>) 
+                : (<SignUp toggle={() => formMode()}/>)}
+    
+             </>
+              )} 
+                  </>
+      </div>
+    </div>
+  )
 }
 
 export default Landing
